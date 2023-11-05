9a3beafa84890ca564bf3ec631811cbaac3585d5
OctoBurr <oduts@RASCOSGT>
2023-11-05 20:18:24 +0000

    Add project files.

---
+++ MonkeJetpack/Plugin.cs
@@ -0,0 +1,64 @@
+﻿using BepInEx;
+using System;
+using System.IO;
+using System.Reflection;
+using UnityEngine;
+using Utilla;
+namespace MonkeJetpack
+{
+    [ModdedGamemode]
+    [BepInDependency("org.legoandmars.gorillatag.utilla")]
+    [BepInPlugin("com.octoburr.gorillatag.monkejetpack", "MonkeJetpack", "1.0.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+        GameObject jetpack;
+        public int speedConfig;
+        bool inModded;
+        void Start() 
+        {
+            Utilla.Events.GameInitialized += OnGameInitialized;
+            jetpackConfig.Initialize();
+        }
+        void OnGameInitialized(object sender, EventArgs e)
+        {
+            AssetBundle bundle = LoadAssetBundle("MonkeJetpack.Resources.jetpack");
+            GameObject asset = bundle.LoadAsset<GameObject>("jetpack");
+            jetpack = Instantiate(asset);
+            jetpack.SetActive(false);
+            jetpack.transform.SetParent(GorillaTagger.Instance.bodyCollider.transform);
+            jetpack.transform.localPosition = new Vector3(0f, 0.05f, -0.25f);
+            jetpack.transform.localRotation = Quaternion.Euler(0f, 180f, 0f);
+            jetpack.transform.localScale = new Vector3(0.15f, 0.15f, 0.15f);
+            speedConfig = jetpackConfig.Speed.Value;
+        }
+        void FixedUpdate()
+        {
+            if (inModded ==  true)
+            {
+                if (ControllerInputPoller.instance.rightControllerPrimaryButton)
+                {
+                    GorillaLocomotion.Player.Instance.bodyCollider.attachedRigidbody.AddForce(Vector3.up * (speedConfig * 100), ForceMode.Acceleration);
+                }
+            }
+        }   
+        [ModdedGamemodeJoin]
+        public void OnJoin(string gamemode)
+        {
+            jetpack.SetActive(true);
+            inModded = true;
+        }
+        [ModdedGamemodeLeave]
+        public void OnLeave(string gamemode)
+        {
+            jetpack.SetActive(false);
+            inModded = false;
+        }
+        public AssetBundle LoadAssetBundle(string path)
+        {
+            Stream stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(path);
+            AssetBundle bundle = AssetBundle.LoadFromStream(stream);
+            stream.Close();
+            return bundle;
+        }
+    }
+}
---
+++ MonkeJetpack/jetpackConfig.cs
@@ -0,0 +1,17 @@
+﻿using BepInEx.Configuration;
+using BepInEx;
+using System.IO;
+
+public static class jetpackConfig
+{
+    public static ConfigFile ConfigFile { get; private set; }
+
+    public static ConfigEntry<int> Speed;
+
+    public static void Initialize()
+    {
+        ConfigFile = new ConfigFile(Path.Combine(Paths.ConfigPath, "MonkeJetpack.cfg"), true);
+
+        Speed = ConfigFile.Bind("Configuration", "Jetpack Speed", 1, "This will determine how fast the jetpack will go.");
+    }
+}
