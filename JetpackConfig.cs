f6a5aa84dc8e89880c5bf3d421dc35c4508e0047
OctoBurr <132259285+OctoBurr@users.noreply.github.com>
2023-11-06 16:47:59 +0000

    Add files via upload

---
+++ JetpackConfig.cs
@@ -0,0 +1,14 @@
+﻿using BepInEx.Configuration;
+using BepInEx;
+using System.IO;
+
+public static class JetpackConfig
+{
+    public static ConfigFile ConfigFile { get; private set; }
+    public static ConfigEntry<float> Speed;
+    public static void Initialize()
+    {
+        ConfigFile = new ConfigFile(Path.Combine(Paths.ConfigPath, "MonkeJetpack.cfg"), true);
+        Speed = ConfigFile.Bind("Configuration", "Jetpack Speed", 1f, "This will determine how fast the jetpack will go.");
+    }
+}
